7e257090fea52cb20f463e8febb72873a5eaf347
Raphael Meyer <raphael.akira.meyer@gmail.com>
2025-12-06 14:09:01 +0100

    🦑 Day 6 Part Two

--- src/aoc/Day06.hs
+++ src/aoc/Day06.hs
@@ -5,27 +5,26 @@ module Day06 where
 import qualified AoC
 import Data.Int
 import qualified Data.List as List
+import qualified Data.List.Extra as Extra
 import qualified Data.Text as Text
 
-data Op = Add | Mult
+data Op = Add | Mult deriving (Eq, Show)
 
 solve :: Text.Text -> AoC.Solution
-solve input = AoC.Solution (partOne problems) "The universe and everything"
-  where
-    problems = parse input
+solve input = AoC.Solution (partOne input) (partTwo input)
 
-partOne :: ([[Int64]], [Op]) -> Text.Text
-partOne = Text.pack . show . uncurry solveMath
+partOne :: Text.Text -> Text.Text
+partOne = Text.pack . show . uncurry solveMath . humanParse
 
--- parse
+partTwo :: Text.Text -> Text.Text
+partTwo = Text.pack . show . uncurry solveMath . cephaloParse
 
-parse :: Text.Text -> ([[Int64]], [Op])
-parse input = (map parseNumbers . init $ rows, parseOps . last $ rows)
-  where
-    rows = Text.lines input
+solveMath :: [[Int64]] -> [Op] -> Int64
+solveMath numbers ops = sum . zipWith calc ops $ numbers
 
-parseNumbers :: Text.Text -> [Int64]
-parseNumbers = map (read . Text.unpack) . filter (not . Text.null) . Text.split (== ' ')
+calc :: Op -> [Int64] -> Int64
+calc Add = sum
+calc Mult = product
 
 parseOps :: Text.Text -> [Op]
 parseOps = map (op . Text.head) . filter (not . Text.null) . Text.split (== ' ')
@@ -36,9 +35,20 @@ parseOps = map (op . Text.head) . filter (not . Text.null) . Text.split (== ' ')
 
 -- part one
 
-solveMath :: [[Int64]] -> [Op] -> Int64
-solveMath numbers ops = sum . zipWith calc ops $ List.transpose numbers
+humanParse :: Text.Text -> ([[Int64]], [Op])
+humanParse input = (List.transpose . map parseHumanNumbers . init $ rows, parseOps . last $ rows)
+  where
+    rows = Text.lines input
 
-calc :: Op -> [Int64] -> Int64
-calc Add = sum
-calc Mult = product
+parseHumanNumbers :: Text.Text -> [Int64]
+parseHumanNumbers = map (read . Text.unpack) . filter (not . Text.null) . Text.split (== ' ')
+
+cephaloParse :: Text.Text -> ([[Int64]], [Op])
+cephaloParse input = (parseCephaloNumbers . Text.transpose . init $ rows, parseOps . last $ rows)
+  where
+    rows = Text.lines input
+
+-- part two
+
+parseCephaloNumbers :: [Text.Text] -> [[Int64]]
+parseCephaloNumbers = map (map (read . Text.unpack)) . Extra.split Text.null . map Text.strip
--- test/Day06Spec.hs
+++ test/Day06Spec.hs
@@ -23,3 +23,25 @@ spec = do
     it "should solve part one" $ do
       let solution = Day06.solve exampleInput
       AoC.one solution `shouldBe` "4277556"
+
+  describe "Part two" $ do
+    it "should solve part two" $ do
+      let solution = Day06.solve exampleInput
+      AoC.two solution `shouldBe` "3263827"
+
+    describe "Parse cephalopod math" $ do
+      let (numbers, ops) = Day06.cephaloParse exampleInput
+
+      it "should parse number of problems" $ do
+        length numbers `shouldBe` 4
+        length ops `shouldBe` 4
+
+      it "should parse vertical numbers" $ do
+        numbers !! 0 `shouldMatchList` [1, 24, 356]
+        numbers !! 1 `shouldMatchList` [369, 248, 8]
+        numbers !! 3 `shouldMatchList` [4, 431, 623]
+
+      it "should parse the operation" $ do
+        ops !! 0 `shouldBe` Day06.Mult
+        ops !! 2 `shouldBe` Day06.Mult
+        ops !! 3 `shouldBe` Day06.Add
