46dba878ebded97f4f5ea7bc93388cc3dcb648e2
Raphael Meyer <raphael.akira.meyer@gmail.com>
2025-12-09 23:14:50 +0100

    🎬 Day 9 Part Two

--- src/aoc/Day09.hs
+++ src/aoc/Day09.hs
@@ -7,17 +7,28 @@ import qualified Data.List as List
 import qualified Data.Text as Text
 
 data Corner = Corner Int Int
+  deriving (Eq, Show)
 
 data Rectangle = Rectangle Corner Corner
+  deriving (Eq, Show)
+
+data Vertical = Vertical {verticalX :: Int, verticalTop :: Int, verticalBottom :: Int}
+  deriving (Eq, Show)
+
+data Horizontal = Horizontal {horizontalY :: Int, horizontalLeft :: Int, horizontalRight :: Int}
+  deriving (Eq, Show)
 
 solve :: Text.Text -> AoC.Solution
-solve input = AoC.Solution (partOne rectangles) "The universe and everything"
+solve input = AoC.Solution (partOne rectangles) (partTwo corners)
   where
     corners = parse input
     rectangles = allRectangles corners
 
 partOne :: [Rectangle] -> Text.Text
 partOne = Text.pack . show . largestArea
+
+partTwo :: [Corner] -> Text.Text
+partTwo = Text.pack . show . largestAreaInside
 
 -- parse input
 
@@ -41,3 +52,104 @@ largestArea = maximum . map rectangleArea
 
 rectangleArea :: Rectangle -> Int
 rectangleArea (Rectangle (Corner ax ay) (Corner bx by)) = (abs (ax - bx) + 1) * (abs (ay - by) + 1)
+
+-- part two
+
+largestAreaInside :: [Corner] -> Int
+largestAreaInside corners = case List.find (isInside verticals horizontals . snd) sorted of
+  Just r -> fst r
+  _ -> undefined
+  where
+    sorted = sortedByArea . normalizeRectangles $ rectangles
+    rectangles = allRectangles corners
+    verticals = allVerticals corners
+    horizontals = allHorizontals corners
+
+isInside :: [Vertical] -> [Horizontal] -> Rectangle -> Bool
+isInside verticals horizontals rectangle =
+  rayCastOnLeft verticals rectangle
+    && noCrossings rectangle verticals horizontals
+
+rayCastOnLeft :: [Vertical] -> Rectangle -> Bool
+rayCastOnLeft verticals rectangle = all (odd . countVerticalsOnPoint onLeft) points
+  where
+    points = verticalPointsToCheck rectangle onLeft
+    onLeft = verticalsOnLeft rectangle . verticalsWithoutBottom $ verticals
+
+noCrossings :: Rectangle -> [Vertical] -> [Horizontal] -> Bool
+noCrossings rectangle verticals horizontals =
+  and
+    [ not . any (crossingVertical rectangle) $ verticals,
+      not . any (crossingVertical rectangle) $ verticals,
+      not . any (crossingHorizontal rectangle) $ horizontals,
+      not . any (crossingHorizontal rectangle) $ horizontals
+    ]
+
+verticalsWithoutBottom :: [Vertical] -> [Vertical]
+verticalsWithoutBottom = map cutBottom
+  where
+    cutBottom (Vertical x top bottom) =
+      if top < bottom
+        then Vertical x top (bottom - 1)
+        else undefined
+
+verticalsOnLeft :: Rectangle -> [Vertical] -> [Vertical]
+verticalsOnLeft (Rectangle (Corner ax _) (Corner _ _)) = filter ((<= ax) . verticalX)
+
+verticalPointsToCheck :: Rectangle -> [Vertical] -> [Int]
+verticalPointsToCheck (Rectangle (Corner _ ay) (Corner _ by)) = foldl isPointToCheck rectangleTopBottom
+  where
+    rectangleTopBottom = if ay < by then [ay, by - 1] else [ay]
+    isPointToCheck points (Vertical _ top bottom) =
+      case (overlap top, overlap bottom) of
+        (True, True) -> top : bottom : points
+        (True, False) -> top : points
+        (False, True) -> bottom : points
+        _ -> points
+    overlap y = ay <= y && y <= by
+
+countVerticalsOnPoint :: [Vertical] -> Int -> Int
+countVerticalsOnPoint verticals point = foldl overlaps 0 verticals
+  where
+    overlaps count (Vertical _ top bottom) =
+      if top <= point && point <= bottom
+        then count + 1
+        else count
+
+crossingVertical :: Rectangle -> Vertical -> Bool
+crossingVertical (Rectangle (Corner ax ay) (Corner bx by)) (Vertical x top bottom) =
+  (ax < x && x < bx)
+    && ((top <= ay && ay < bottom) || (top < by && by <= bottom))
+
+crossingHorizontal :: Rectangle -> Horizontal -> Bool
+crossingHorizontal (Rectangle (Corner ax ay) (Corner bx by)) (Horizontal y left right) =
+  (ay < y && y < by)
+    && ((left <= ax && ax < right) || (left < bx && bx <= right))
+
+allVerticals :: [Corner] -> [Vertical]
+allVerticals corners = foldl mkVertical [] $ zip corners (last corners : init corners)
+  where
+    mkVertical verticals (Corner ax ay, Corner bx by)
+      | ax == bx && ay < by = Vertical ax ay by : verticals
+      | ax == bx = Vertical ax by ay : verticals
+      | otherwise = verticals
+
+allHorizontals :: [Corner] -> [Horizontal]
+allHorizontals corners = foldl mkHorizontal [] $ zip corners (last corners : init corners)
+  where
+    mkHorizontal horizontals (Corner ax ay, Corner bx by)
+      | ay == by && ax < bx = Horizontal ay ax bx : horizontals
+      | ay == by = Horizontal ay bx ax : horizontals
+      | otherwise = horizontals
+
+sortedByArea :: [Rectangle] -> [(Int, Rectangle)]
+sortedByArea = List.sortBy (\a b -> compare (fst b) (fst a)) . map (\r -> (rectangleArea r, r))
+
+normalizeRectangles :: [Rectangle] -> [Rectangle]
+normalizeRectangles = map normalize
+  where
+    normalize (Rectangle (Corner ax ay) (Corner bx by)) = case (ax < bx, ay < by) of
+      (True, True) -> Rectangle (Corner ax ay) (Corner bx by)
+      (True, False) -> Rectangle (Corner ax by) (Corner bx ay)
+      (False, True) -> Rectangle (Corner bx ay) (Corner ax by)
+      (False, False) -> Rectangle (Corner bx by) (Corner ax ay)
--- test/Day09Spec.hs
+++ test/Day09Spec.hs
@@ -21,6 +21,62 @@ exampleInput =
       "7,3"
     ]
 
+example2 :: Text.Text
+example2 =
+  Text.intercalate
+    "\n"
+    [ "1,3",
+      "1,7",
+      "20,7",
+      "20,1",
+      "8,1",
+      "8,5",
+      "2,5",
+      "2,3"
+    ]
+
+--
+--         #-----------------------#
+--         |                       |
+--      #--#                       |
+--      |                          #--#
+--  #---#                             |
+--  |                                 #--#
+--  #----------------------------#       |
+--                               |       |
+--   #---------------------------#       |
+--   |                                   |
+--   #-------#                 #---------#
+--           #-----------------#
+--
+-- 12,39 - 34,45 -> 161
+
+example3 :: Text.Text
+example3 =
+  Text.intercalate
+    "\n"
+    [ "12,39",
+      "12,41",
+      "9,41",
+      "9,43",
+      "5,43",
+      "5,45",
+      "34,45",
+      "34,47",
+      "6,47",
+      "6,49",
+      "14,49",
+      "14,50",
+      "32,50",
+      "32,49",
+      "42,49",
+      "42,44",
+      "39,44",
+      "39,42",
+      "36,42",
+      "36,39"
+    ]
+
 spec :: Spec
 spec = do
   let input = Day09.parse exampleInput
@@ -34,3 +90,56 @@ spec = do
     it "should solve example part one" $ do
       let solution = Day09.solve exampleInput
       AoC.one solution `shouldBe` "50"
+
+  describe "Part two" $ do
+    it "should solve example part two" $ do
+      let solution = Day09.solve exampleInput
+      AoC.two solution `shouldBe` "24"
+
+    it "should solve part two example 2" $ do
+      let solution = Day09.solve example2
+      AoC.two solution `shouldBe` "91"
+
+    it "should solve part two example 3" $ do
+      let solution = Day09.solve example3
+      AoC.two solution `shouldBe` "161"
+
+    it "should find all vertical lines" $ do
+      let verticals = Day09.allVerticals input
+      length verticals `shouldBe` 4
+      verticals `shouldSatisfy` elem (Day09.Vertical 2 3 5)
+      verticals `shouldSatisfy` elem (Day09.Vertical 11 1 7)
+
+      let verticals3 = Day09.allVerticals . Day09.parse $ example3
+      length verticals3 `shouldBe` 10
+      verticals3 `shouldSatisfy` elem (Day09.Vertical 6 47 49)
+      verticals3 `shouldSatisfy` elem (Day09.Vertical 39 42 44)
+
+    it "should normalize rectangles" $ do
+      let normalized = Day09.normalizeRectangles . Day09.allRectangles $ input
+      normalized `shouldSatisfy` elem (Day09.Rectangle (Day09.Corner 2 1) (Day09.Corner 11 3))
+      normalized `shouldNotSatisfy` elem (Day09.Rectangle (Day09.Corner 2 3) (Day09.Corner 11 1))
+      normalized `shouldNotSatisfy` elem (Day09.Rectangle (Day09.Corner 11 1) (Day09.Corner 2 3))
+      normalized `shouldNotSatisfy` elem (Day09.Rectangle (Day09.Corner 11 3) (Day09.Corner 2 1))
+
+    it "should sort rectangles by area" $ do
+      let rectangles = Day09.sortedByArea . Day09.normalizeRectangles . Day09.allRectangles $ input
+      (fst . head $ rectangles) `shouldBe` 50
+      (fst . last $ rectangles) `shouldBe` 3
+
+    it "should check if rectangle is inside" $ do
+      let corners = Day09.parse example3
+          verticals = Day09.allVerticals corners
+          horizontals = Day09.allHorizontals corners
+      Day09.Rectangle (Day09.Corner 12 39) (Day09.Corner 34 45) `shouldSatisfy` Day09.isInside verticals horizontals
+      Day09.Rectangle (Day09.Corner 6 47) (Day09.Corner 32 49) `shouldSatisfy` Day09.isInside verticals horizontals
+      Day09.Rectangle (Day09.Corner 34 39) (Day09.Corner 36 45) `shouldSatisfy` Day09.isInside verticals horizontals
+
+    it "should check if rectangle is not inside" $ do
+      let corners = Day09.parse example3
+          verticals = Day09.allVerticals corners
+          horizontals = Day09.allHorizontals corners
+      Day09.Rectangle (Day09.Corner 9 42) (Day09.Corner 32 49) `shouldNotSatisfy` Day09.isInside verticals horizontals
+      Day09.Rectangle (Day09.Corner 32 42) (Day09.Corner 36 49) `shouldNotSatisfy` Day09.isInside verticals horizontals
+      Day09.Rectangle (Day09.Corner 9 39) (Day09.Corner 12 43) `shouldNotSatisfy` Day09.isInside verticals horizontals
+      Day09.Rectangle (Day09.Corner 6 45) (Day09.Corner 34 47) `shouldNotSatisfy` Day09.isInside verticals horizontals
